1b41031c1ab6cf548d268c7aadbd0808b758c6a1
lakshven <87704460+lakshven@users.noreply.github.com>
2026-03-12 16:58:47 +0000

    Change axios baseURL to production endpoint
    Url Changes

--- amsfrontend/src/utils/axiosInstance.ts
+++ amsfrontend/src/utils/axiosInstance.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 
 // ✅ Create the axios instance
 const instance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://www.ampt.online/api"
   withCredentials: false, // ⭐ important if backend uses cookies
   headers: { "Content-Type": "application/json" },
 });
