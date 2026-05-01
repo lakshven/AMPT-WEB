fd99c550f2dd3bd48f0ed704c99b955010c01afa
root <root@ecsl-vmtiNHNDYT.dizm5bho15zupa3qmyjibpgqjg.cwx.internal.cloudapp.net>
2026-05-01 21:10:16 +0000

    Fix updateWorkItem controller (clean empty values, prevent NaN/undefined)

--- amsbackend/src/controllers/assets/workItems/updateWorkItem.ts
+++ amsbackend/src/controllers/assets/workItems/updateWorkItem.ts
@@ -6,6 +6,9 @@ export const updateWorkItem = async (req: Request, res: Response) => {
   try {
     const id = Number(req.params.id);
 
+    const clean = (value: any) =>
+      value === "" || value === null || value === undefined ? undefined : value;
+
     const {
       work_item,
       possible_consequence,
@@ -19,37 +22,45 @@ export const updateWorkItem = async (req: Request, res: Response) => {
       status
     } = req.body;
 
-   const data: any = {};
+    const data: any = {};
 
-   if (work_item !== undefined) data.work_item = work_item;
-   if (possible_consequence !== undefined) data.possible_consequence = possible_consequence;
-   if (risk_mitigation_proposals !== undefined) data.risk_mitigation_proposals = risk_mitigation_proposals;
-   if (status !== undefined) data.status = status;
+    if (clean(work_item) !== undefined) data.work_item = work_item;
+    if (clean(possible_consequence) !== undefined) data.possible_consequence = possible_consequence;
+    if (clean(risk_mitigation_proposals) !== undefined) data.risk_mitigation_proposals = risk_mitigation_proposals;
+    if (clean(status) !== undefined) data.status = status;
 
-   if (current_likelihood !== undefined && current_severity !== undefined) {
-    data.current_likelihood = Number(current_likelihood);
-    data.current_severity = Number(current_severity);
-    data.current_rating = Number(current_likelihood) * Number(current_severity);
-   }
+    const cl = clean(current_likelihood);
+    const cs = clean(current_severity);
 
-   if (current_date_logged !== undefined) {
-    data.current_date_logged = new Date(current_date_logged);
-   }
+    if (cl !== undefined && cs !== undefined) {
+      data.current_likelihood = Number(cl);
+      data.current_severity = Number(cs);
+      data.current_rating = Number(cl) * Number(cs);
+    }
 
-   if (mitigation_likelihood !== undefined && mitigation_severity !== undefined) {
-    data.mitigation_likelihood = Number(mitigation_likelihood);
-    data.mitigation_severity = Number(mitigation_severity);
-    data.mitigation_rating = Number(mitigation_likelihood) * Number(mitigation_severity);
-   }
+    if (clean(current_date_logged) !== undefined) {
+      data.current_date_logged = new Date(current_date_logged);
+    }
 
-   if (mitigation_completion !== undefined) {
-    data.mitigation_completion = mitigation_completion ? new Date(mitigation_completion) : null;
-   }
+    const ml = clean(mitigation_likelihood);
+    const ms = clean(mitigation_severity);
 
-   const updated = await prismaClient().workItem.update({
-    where: { id },
-    data
-   });
+    if (ml !== undefined && ms !== undefined) {
+      data.mitigation_likelihood = Number(ml);
+      data.mitigation_severity = Number(ms);
+      data.mitigation_rating = Number(ml) * Number(ms);
+    }
+
+    if (mitigation_completion !== undefined) {
+      data.mitigation_completion =
+        mitigation_completion ? new Date(mitigation_completion) : null;
+    }
+
+    const updated = await prismaClient().workItem.update({
+      where: { id },
+      data
+    });
+
     res.json({ success: true, workItem: updated });
   } catch (err) {
     console.error("Update work item error:", err);
